d9b77a210b61598455551104714f3224ddbd1488
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-10-12 15:06:05 +0530

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
+import localFont from "next/font/local"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], variable: "--inter" })
+
+const clash = localFont({
+  src: "../public/fonts/clash.ttf",
+})
 
 export const metadata = {
   title: "Shooketh – AI bot fine-tuned on Shakespeare's literary works",
@@ -17,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${clash.className} ${inter.variable}`}>
         {children}
         <Analytics />
       </body>
